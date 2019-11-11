22f4759bac0158e52e34721ddfd3daceaea9740a
brandon <otoolb@gmail.com>
2019-11-11 09:47:33 -0800

    Refactor Touch Input and fix for ipad

--- app/assets/TouchInput.js
+++ app/assets/TouchInput.js
@@ -1,82 +1,96 @@
+import { Browser } from "./Browser.js";
+
 export class TouchInput {
     constructor(surface) {
-        if ('ontouchstart' in window) {
-            surface.addEventListener("touchstart", handleAll, false);
-            surface.addEventListener("touchend", handleAll, false);
-            surface.addEventListener("touchcancel", handleAll, false);
-            surface.addEventListener("touchmove", handleAll, false);
-        }
-    }
-}
+        if (true || 'ontouchstart' in window) {
+            //console.log("we add the touch stuff");
+            this.surface = surface;
+            this.width = 25;
+            this.height = 25;
 
-/*
-function getPens(touchList) {
-    let i;
-    let pens = [];
-    for (i=0; i<touchList.length; i++) {
-        let touch = touchList[i];
-        if (touch.touchType == "stylus") {
-            pens.push(touch);
+            surface.addEventListener("touchstart", this.handleAll.bind(this), false);
+            surface.addEventListener("touchend", this.handleAll.bind(this), false);
+            surface.addEventListener("touchcancel", this.handleAll.bind(this), false);
+            surface.addEventListener("touchmove", this.handleAll.bind(this), false);
         }
     }
 
-    //return i;
-    return pens;
-}
+    handleAll(e) {
+        e.preventDefault();
 
-function testEraser(touch) {
-    let x = resolution*touch.clientX;
-    let y = resolution*touch.clientY;
-    return (x > width-resolution*100) && (y > height-resolution*100);
-}
+        let pens = this.getPens(e.touches);
+        let erasers = this.getErasers(e.touches);
 
-function getErasers(touchList) {
-    let enabled = false;
-    let erasers = [];
-
-    let i;
-    for (i=0; i<touchList.length; i++) {
-        let touch = touchList[i];
-        if (enabled) {
-            erasers.push(touch);
-        }
-        enabled = enabled || testEraser(touch);
-    }
-
-    return erasers;
-}
-
-function handleAll(e) {
-    e.preventDefault();
-
-    let pens = getPens(e.touches);
-    let erasers = getErasers(e.touches);
-
-    if (pens.length > 0) {
-        let pen = pens[0];
-        let pt = [ resolution*pen.clientX, resolution*pen.clientY ];
-        stylus.id = pen.identifier;
-        stylus.pressure = pen.force;
-        stylus.draw(fCtx, pt);
-    } else if (erasers.length > 0){
-        for (let eraser of erasers) {
-            let pt = [ resolution*eraser.clientX, resolution*eraser.clientY ];
-            stylus.erase(fCtx, pt);
+        if (pens.length > 0) {
+            let pen = pens[0];
+            let pt = [ Browser.resolution*pen.clientX, Browser.resolution*pen.clientY ];
+            //this.surface.pen.id = pen.identifier;
+            //this.surface.pen.pressure = pen.force;
+            this.surface.logMove(pen.identifier, pt, 1, [0.1, 0.1]);
+            this.surface.mouseDraw(pen.identifier, pt);
+        } else if (erasers.length > 0){
+            for (let eraser of erasers) {
+                let pt = [ Browser.resolution*eraser.clientX, Browser.resolution*eraser.clientY ];
+                this.surface.pen.erase(this.surface.fCtx, pt);
+            }
         }
     }
-}
 
-function touchHere(tp) {
-    if (tp.touchType=="stylus") {
-        let t3 = [ resolution*tp.clientX, resolution*tp.clientY ];
-        stylus.id = tp.identifier;
-        stylus.type = "pen";
-        stylus.tip = "pen";
 
-        stylus.pressure = tp.force;
-        stylus.draw(fCtx, t3);
-    } else if (tp.touchType=="touch" && touchEnabled) {
-        // touch support
+    getPens(touchList) {
+        let i;
+        let pens = [];
+        for (i=0; i<touchList.length; i++) {
+            let touch = touchList[i];
+            if (!touch.id || touch.touchType == "stylus") {
+                return [touch];
+            }
+        }
+
+        for (i=0; i<touchList.length; i++) {
+            let touch = touchList[i];
+            if (!touch.touchType || touch.touchType == "stylus") {
+                pens.push(touch);
+            }
+        }
+
+        return pens;
+    }
+
+    testEraser(touch) {
+        let x = Browser.resolution*touch.clientX;
+        let y = Browser.resolution*touch.clientY;
+        return (x > this.width - Browser.resolution*100) && (y > this.height - Browser.resolution*100);
+    }
+
+    getErasers(touchList) {
+        let enabled = false;
+        let erasers = [];
+
+        let i;
+        for (i=0; i<touchList.length; i++) {
+            let touch = touchList[i];
+            if (enabled) {
+                erasers.push(touch);
+            }
+            enabled = enabled || this.testEraser(touch);
+        }
+
+        return erasers;
+    }
+
+    touchHere(tp) {
+        if (tp.touchType=="stylus") {
+            let t3 = [ Browser.resolution*tp.clientX, Browser.resolution*tp.clientY ];
+            this.surface.pen.id = tp.identifier;
+            this.surface.pen.type = "pen";
+            this.surface.pen.tip = "pen";
+
+            this.surface.pen.pressure = tp.force;
+            // TODO fix width
+            this.surface.pen.draw(fCtx, t3, 5);
+        } else if (tp.touchType=="touch" && touchEnabled) {
+            // touch support
+        }
     }
 }
-*/
